2ef4bd359eb5651c56f5a5a4bfd6d762b8af1716
Adrián <accarmona@gmail.com>
2015-08-12 11:03:28 -0600

    se corrige prueba

--- NewFeatures2015VS/Negocio_Tests/InversionEspecial/ObtenerCalculoInteresesNetos_Tests.vb
+++ NewFeatures2015VS/Negocio_Tests/InversionEspecial/ObtenerCalculoInteresesNetos_Tests.vb
@@ -6,10 +6,11 @@ Imports Negocio
 
     <TestMethod()> Public Sub ObtenerCalculoInteresesNetos_EscenarioRedondeo_DatosRedondeoHaciaDigitoMenor()
 
-        Dim resultadoEsperado = 0.12
+        Dim resultadoEsperado = 0.0375D
         Dim interesSimple As Decimal = 0.25
         Dim plazoInversion As Decimal = 0.15
 
+        Dim dato = interesSimple * plazoInversion
         Dim resultadoObtenido As Decimal
 
         resultadoObtenido = CalculadorDeInteresInversion.ObtenerCalculoInteresesNetos(interesSimple, plazoInversion)
