13a99e36fbf5917f0eb99fd2382ffbd8b9fc94a7
Adrián <accarmona@gmail.com>
2015-08-13 15:55:08 -0600

    se agregan relaciones

--- NewFeatures2015VS/Negocio_Tests/InversionEspecial/ObtenerCalculoInteresesNetos_Tests.vb
+++ NewFeatures2015VS/Negocio_Tests/InversionEspecial/ObtenerCalculoInteresesNetos_Tests.vb
@@ -6,17 +6,17 @@ Imports Negocio
 
     <TestMethod()> Public Sub ObtenerCalculoInteresesNetos_EscenarioRedondeo_DatosRedondeoHaciaDigitoMenor()
 
-        'Dim resultadoEsperado = 0.045D
-        'Dim interesSimple As Decimal = 0.25
-        'Dim plazoInversion As Decimal = 0.18
+        Dim resultadoEsperado = 0.045D
+        Dim interesSimple As Decimal = 0.25
+        Dim plazoInversion As Decimal = 0.18
 
-        'Dim dato = interesSimple * plazoInversion
-        'Dim resultadoObtenido As Decimal
-        'Dim elemento As New Estudiante 'With {.Descripcion = Nothing, .Id = 1, .Nombre = "Nombre"}
-        'elemento = Nothing
-        'Dim nombre = elemento?.Descripcion?.ToString()
-        'resultadoObtenido = CalculadorDeInteresInversion.ObtenerCalculoInteresesNetos(interesSimple, plazoInversion)
-        'Assert.AreEqual(resultadoEsperado, resultadoObtenido)
+        Dim dato = interesSimple * plazoInversion
+        Dim resultadoObtenido As Decimal
+        Dim elemento As New Estudiante 'With {.Descripcion = Nothing, .Id = 1, .Nombre = "Nombre"}
+        elemento = Nothing
+        Dim nombre = elemento?.Descripcion?.ToString()
+        resultadoObtenido = CalculadorDeInteresInversion.ObtenerCalculoInteresesNetos(interesSimple, plazoInversion)
+        Assert.AreEqual(resultadoEsperado, resultadoObtenido)
 
     End Sub
 
