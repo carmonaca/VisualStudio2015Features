151e91393035826aa15ba59d1583876219c9f7d6
Adrián <accarmona@gmail.com>
2015-08-13 15:30:56 -0600

    se agrega clase de pruebas para ejemplo de creación de clases con IDE

---
+++ NewFeatures2015VS/Negocio_Tests/EjemploCreacionClases_Tests.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Text
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass()>
+Public Class EjemploCreacionClases_Tests
+
+    Private testContextInstance As TestContext
+
+
+    Public Property TestContext() As TestContext
+        Get
+            Return testContextInstance
+        End Get
+        Set(ByVal value As TestContext)
+            testContextInstance = Value
+        End Set
+    End Property
+
+
+
+
+End Class
