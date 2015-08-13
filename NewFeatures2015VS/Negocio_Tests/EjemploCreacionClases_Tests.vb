80d5e51b6ba0beec19d0c092132c240138a09517
Adrián <accarmona@gmail.com>
2015-08-13 16:06:45 -0600

    datos para creación

--- NewFeatures2015VS/Negocio_Tests/EjemploCreacionClases_Tests.vb
+++ NewFeatures2015VS/Negocio_Tests/EjemploCreacionClases_Tests.vb
@@ -11,7 +11,15 @@ Public Class EjemploCreacionClases_Tests
     <TestMethod>
     Public Sub EsPosibleReintentarPagoMovil_CasoBasico_Si()
 
+        'Dim elResultadoEsperado As Boolean = True
 
+        'Dim reintentos As Integer = 0
+
+        'Dim validadorPagos As New ReintentarPagoMovil
+
+        'Dim elResultadoObtenido = validadorPagos.EsPosibleReintentarElPago(reintentos)
+
+        'Assert.AreEqual(elResultadoEsperado, elResultadoObtenido)
 
     End Sub
 
