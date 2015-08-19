bd694cf7787002888f231950ee58e256895e0d7a
Adrián <accarmona@gmail.com>
2015-08-19 10:01:57 -0600

    ejemplo en comentarios para mostrar nullable properties en niveles más profundos

--- NewFeatures2015VS/AplicaciónVisualStudio2015/PropiedadesNullables.vb
+++ NewFeatures2015VS/AplicaciónVisualStudio2015/PropiedadesNullables.vb
@@ -9,6 +9,9 @@ Module PropiedadesNullables
         Dim estudiante As Estudiante
         estudiante = Nothing
 
+        'estudiante = as new Estudiante
+        'estudiante.CentroEducativo = Nothing
+
         If String.IsNullOrEmpty(estudiante.Nombre) Then
             Console.WriteLine("Nombre de estudiante sin contenido")
             Console.ReadLine()
