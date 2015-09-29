add4ca15eb7ce2cdf2dcdc705d5153aa404d14ca
Adrián <accarmona@gmail.com>
2015-09-29 15:45:45 -0600

    se agrega nueva prueba calculo intereses

--- NewFeatures2015VS/Negocio_Tests/InversionEspecial/ObtenerCalculoInteresesNetos_Tests.vb
+++ NewFeatures2015VS/Negocio_Tests/InversionEspecial/ObtenerCalculoInteresesNetos_Tests.vb
@@ -20,4 +20,22 @@ Imports Negocio
 
     End Sub
 
+
+
+    <TestMethod()> Public Sub ObtenerCalculoInteresesNetos_EscenarioRedondeo_PruebaNueva()
+
+        Dim resultadoEsperado = 0.045D
+        Dim interesSimple As Decimal = 0.25
+        Dim plazoInversion As Decimal = 0.18
+
+        Dim dato = interesSimple * plazoInversion
+        Dim resultadoObtenido As Decimal
+        Dim elemento As New Estudiante 'With {.Descripcion = Nothing, .Id = 1, .Nombre = "Nombre"} 
+        elemento = Nothing
+        Dim nombre = elemento?.Descripcion?.ToString()
+        resultadoObtenido = CalculadorDeInteresInversion.ObtenerCalculoInteresesNetos(interesSimple, plazoInversion)
+        Assert.AreEqual(resultadoEsperado, resultadoObtenido)
+
+    End Sub
+
 End Class
